58ab174561f37ce28715610d3f415f4ebc6c2e5c
Driexus <toliasdimitris@gmail.com>
2024-02-05 20:31:11 +0200

    Closes #28

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -4,6 +4,7 @@ import 'package:simplemoneytracker/model/money_entry.dart';
 import 'package:simplemoneytracker/repos/money_entry_repo.dart';
 import 'package:simplemoneytracker/ui/home/add_activity/colors_list.dart';
 import 'package:simplemoneytracker/ui/home/home_page.dart';
+import 'package:simplemoneytracker/ui/shared/overscroll_notification_listener.dart';
 import 'package:simplemoneytracker/ui/timeline/timeline_page.dart';
 
 import 'cubits/activities_cubit.dart';
@@ -22,44 +23,46 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              toolbarHeight: 0, // Remove extra space on top of tabs
-              bottom: const TabBar(
-                tabs: [
-                  Tab(icon: Icon(Icons.directions_car)),
-                  Tab(icon: Icon(Icons.directions_transit)),
-                  Tab(icon: Icon(Icons.directions_bike)),
-                ],
-              ),
-            ),
-            body: MultiBlocProvider(
-                providers: [
-                  BlocProvider(
-                    create: (_) => EntriesBloc(MainPage._repo)..add(
-                        FiltersUpdated(
-                          MoneyEntryFilters(
-                            allowedTypes: [MoneyType.expense]
-                          )
-                        )
-                    ),
-                  ),
-                  BlocProvider(
-                    create: (_) => ActivitiesCubit(),
-                  ),
-                ],
-                child: const TabBarView(
-                  children: [
-                    HomePage(),
-                    TimelinePage(),
-                    ColorsList(),
+    return OverscrollNotificationListener(
+        child: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                toolbarHeight: 0, // Remove extra space on top of tabs
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.directions_car)),
+                    Tab(icon: Icon(Icons.directions_transit)),
+                    Tab(icon: Icon(Icons.directions_bike)),
                   ],
-                )
-            )
-        )
+                ),
+              ),
+              body: MultiBlocProvider(
+                  providers: [
+                    BlocProvider(
+                      create: (_) => EntriesBloc(MainPage._repo)..add(
+                          FiltersUpdated(
+                            MoneyEntryFilters(
+                              allowedTypes: [MoneyType.expense]
+                            )
+                          )
+                      ),
+                    ),
+                    BlocProvider(
+                      create: (_) => ActivitiesCubit(),
+                    ),
+                  ],
+                  child: const TabBarView(
+                    children: [
+                      HomePage(),
+                      TimelinePage(),
+                      ColorsList(),
+                    ],
+                  )
+              )
+          )
+      )
     );
   }
 }
